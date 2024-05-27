e4a3b5a151d1ec6c5a107ddb959a7fb9dc71bddf
manonaSamir <bena.yalla@yahoo.com>
2024-05-27 13:26:09 +0300

    add clinic Page

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,11 @@ import os
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_principal import Principal
+from flask_uploads import UploadSet, IMAGES, configure_uploads
+from werkzeug.utils import secure_filename  # Updated import
 import uuid
+
+
 
 load_dotenv()
 app = Flask(__name__)
@@ -18,13 +22,28 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqldb://{IDOC_USER}:{IDOC_PWD}
 # app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{IDOC_USER}:{IDOC_PWD}@{IDOC_HOST}/{IDOC_DB}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'ad983778da711747f7cb3e3b'
+# app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(os.getcwd(), 'uploads')
+
+
 db = SQLAlchemy(app)
 app.config['CACHE_ID'] = str(uuid.uuid4())
 
+#Configure flask_bcrypt
 bcrypt= Bcrypt(app)
+
+#Configure flask_login
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
 
+# Configure Flask-Uploads
+# photos = UploadSet('images', IMAGES)
+app.config['UPLOAD_FOLDER'] = os.path.join('app','static', 'images', 'clinic')
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+
+# configure_uploads(app, photos)
+
+#Configure flask_principal
 principal = Principal(app)
 
 
