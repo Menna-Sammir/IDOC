0aa8917379b088a0a52e4d26ab51a0c79dcc02e0
manonaSamir <bena.yalla@yahoo.com>
2024-08-28 22:40:07 +0300

    update search jinja

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from app import app
 # from cryptography.hazmat.backends import default_backend
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
 
 #backend = default_backend()
