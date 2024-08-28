4bfaf4b26cd20577d0d9051bc71ea8aa9a58153a
fatma soliman <fatmacivil@hotmail.com>
2024-08-28 22:43:21 +0300

    push to pull2

--- app/views/doctor.py
+++ app/views/doctor.py
@@ -70,6 +70,123 @@ def doctor_dash():
     )
 
 
+@app.route('/doctor_profile', methods=['GET', 'POST'])
+@login_required
+@doctor_permission.require(http_exception=403)
+def doctor_profile():
+    specializations = Specialization.query.filter().all()
+    doctor = Doctor.query.filter_by(user_id=current_user.id).first_or_404()
+    user = User.query.filter_by(id=current_user.id).first_or_404()
+    doctor_form = EditDoctorForm(obj=doctor)
+    user_form = EditUserForm(obj=current_user)
+
+    doctor_form.specialization_id.choices = [
+        ('', translate('Select a specialization'))
+    ] + [
+        (specialization.id, translate(specialization.specialization_name))
+        for specialization in specializations
+    ]
+
+    if request.method == 'POST':
+        if doctor_form.validate_on_submit():
+            try:
+                doctor_form.populate_obj(doctor)
+                user_form.populate_obj(user)
+                user.name = (user_form.firstname.data + ' ' + user_form.lastname.data)
+                doc_dur = int(doctor_form.duration.data) if doctor_form.duration.data is not None else 0
+                doctor.duration = str(doc_dur * 100)
+                file = request.files['photo']
+                if file.filename:
+                    #check from js code
+                    if 'photo' in request.files:
+                        unique_str = str(uuid.uuid4())[:8]
+                        original_filename, extension = os.path.splitext(file.filename)
+                        new_filename = f"{unique_str}_{current_user.name.replace(' ', '_')}{extension}"
+                        user.photo = new_filename
+                        if file and allowed_file(file.filename):
+                            filename = secure_filename(new_filename)
+                            file.save(
+                                os.path.join(
+                                    app.config['UPLOAD_FOLDER'], 'doctors', filename
+                                )
+                            )
+                db.session.commit()
+                flash('Data update successfully', category='success')
+                return redirect(url_for('doctor_profile'))
+            except Exception as e:
+                flash(f'something wrong', category='danger')
+                print(str(e))
+        if doctor_form.errors != {}:
+            for err_msg in doctor_form.errors.values():
+                flash(
+                    f'there was an error with creating a user: {err_msg}',
+                    category='danger'
+                )
+        elif user_form.errors != {}:
+            for err_msg in user_form.errors.values():
+                flash(
+                    f'there was an error with creating a user: {err_msg}',
+                    category='danger'
+                )
+        return redirect(url_for('doctor_profile'))
+    name_split = current_user.name.split()
+    user_form.firstname.data = name_split[0]
+    user_form.lastname.data = ' '.join(name_split[1:])
+    minutes = doctor.duration.hour * 60 + doctor.duration.minute
+    doctor_form.duration.data = str(minutes)
+    return render_template(
+        'doctor-profile-settings.html', user_form=user_form, doctor_form=doctor_form
+    )
+
+@app.route('/Prescription', methods=['GET', 'POST'])
+@login_required
+@doctor_permission.require(http_exception=403)
+def add_prescription():
+    form = AddMedicineForm()
+    patient_id = "f6f977bc-2c89-4dfc-94f2-48251e5f52dd"
+    # patient_id = request.args.get("patient_id")
+    if form.validate_on_submit():
+        try:
+            # MedicineTimes
+            for item in form.items:
+                med_exist = PatientMedicine.query.filter_by(medName=item.form.name.data).first()
+                if not med_exist:
+                    Patient_Medicine = PatientMedicine(
+                        medName=item.form.name.data,
+                        Quantity=item.form.quantity.data,
+                        Days=item.form.Days.data,
+                        patient_id=patient_id
+                    )
+                    db.session.add(Patient_Medicine)
+                    current_medicine = Patient_Medicine
+                    flash('Medicine added successfully', category='success')
+
+                else:
+                    med_exist.Quantity = item.form.quantity.data
+                    med_exist.Days = item.form.Days.data
+                    current_medicine = med_exist
+                    MedicineTimes.query.filter_by(patient_medicine=med_exist).delete()
+                    flash('Medicine updated successfully', category='success')
+            for time_of_day in item.form.time_of_day.data:
+                Medicine_Times = MedicineTimes(
+                    patient_medicine=current_medicine,
+                    time_of_day=time_of_day
+                )
+                db.session.add(Medicine_Times)
+            db.session.commit()
+            return redirect(url_for('add_prescription'))
+        except Exception as e:
+            db.session.rollback()
+            flash(f'There was an error: {e}', category='danger')
+
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash(f'There was an error with adding medicine: {err_msg}', category='danger')
+
+    return render_template('prescription.html', form=form)
+
+
+
 ### patient list
 @app.route('/doctor_dashboard/patient_list', methods=['GET', 'POST'])
 @login_required
