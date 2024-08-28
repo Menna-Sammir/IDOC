5b830cbd101a8a474861abed18868cd22ab9be2e
fatma soliman <fatmacivil@hotmail.com>
2024-08-28 22:41:10 +0300

    fix search

--- app/views/doctor.py
+++ app/views/doctor.py
@@ -19,13 +19,29 @@ clinic_permission = Permission(RoleNeed('clinic'))
 # doctor search page
 @app.route('/search_doctor', methods=['GET', 'POST'])
 def search_doctor():
+    
+    specialization_id = session.get('specialization_id', None)
+    governorate_id = session.get('governorate_id', None)
+    doctor_name = session.get('doctor_name', None)
+    
+ 
     page = request.args.get('page', 1, type=int)
-    per_page = 10 
+    per_page = 10
+
     form = AppointmentForm()
+    
     query = db.session.query(Doctor, Specialization, Clinic, Governorate) \
         .join(Specialization, Doctor.specialization_id == Specialization.id) \
         .join(Clinic, Doctor.clinic_id == Clinic.id) \
         .join(Governorate, Clinic.governorate_id == Governorate.id)
+
+   
+    if specialization_id:
+        query = query.filter(Doctor.specialization_id == specialization_id)
+    if governorate_id:
+        query = query.filter(Clinic.governorate_id == governorate_id)
+    if doctor_name:
+        query = query.filter(Doctor.name.ilike(f'%{doctor_name}%'))
 
     specializations = Specialization.query.all()
     governorates = Governorate.query.all()
@@ -33,13 +49,14 @@ def search_doctor():
     selected_specializations = []
     selected_date = None
 
+   
     if request.method == 'POST':
         selected_specializations = request.form.getlist('select_specialization')
         selected_date = request.form.get('date')
 
         if selected_specializations:
             query = query.filter(Doctor.specialization_id.in_(selected_specializations))
-        
+
         if selected_date:
             search_date = datetime.strptime(selected_date, '%d/%m/%Y').date()
             subquery = db.session.query(Doctor.id).outerjoin(Appointment, and_(
@@ -48,17 +65,63 @@ def search_doctor():
             )).filter(Appointment.id == None)
 
             query = query.filter(Doctor.id.in_(subquery))
-    
+
+
     pagination = query.paginate(page=page, per_page=per_page, error_out=False)
     doctors = pagination.items
+
+
     print(f"Doctors: {doctors}")
     print(f"Generated SQL Query: {query}")
-    return render_template('search.html', doctors=doctors, 
+
+    return render_template('search.html', doctors=doctors,
                            specializations=specializations, governorates=governorates,
                            selected_specializations=selected_specializations,
                            selected_date=selected_date,
                            pagination=pagination,
                            form=form)
+    
+# def search_doctor():
+#     page = request.args.get('page', 1, type=int)
+#     per_page = 10 
+#     form = AppointmentForm()
+#     query = db.session.query(Doctor, Specialization, Clinic, Governorate) \
+#         .join(Specialization, Doctor.specialization_id == Specialization.id) \
+#         .join(Clinic, Doctor.clinic_id == Clinic.id) \
+#         .join(Governorate, Clinic.governorate_id == Governorate.id)
+
+#     specializations = Specialization.query.all()
+#     governorates = Governorate.query.all()
+
+#     selected_specializations = []
+#     selected_date = None
+
+#     if request.method == 'POST':
+#         selected_specializations = request.form.getlist('select_specialization')
+#         selected_date = request.form.get('date')
+
+#         if selected_specializations:
+#             query = query.filter(Doctor.specialization_id.in_(selected_specializations))
+        
+#         if selected_date:
+#             search_date = datetime.strptime(selected_date, '%d/%m/%Y').date()
+#             subquery = db.session.query(Doctor.id).outerjoin(Appointment, and_(
+#                 Doctor.id == Appointment.doctor_id,
+#                 func.date(Appointment.date) == search_date
+#             )).filter(Appointment.id == None)
+
+#             query = query.filter(Doctor.id.in_(subquery))
+    
+#     pagination = query.paginate(page=page, per_page=per_page, error_out=False)
+#     doctors = pagination.items
+#     print(f"Doctors: {doctors}")
+#     print(f"Generated SQL Query: {query}")
+#     return render_template('search.html', doctors=doctors, 
+#                            specializations=specializations, governorates=governorates,
+#                            selected_specializations=selected_specializations,
+#                            selected_date=selected_date,
+#                            pagination=pagination,
+#                            form=form)
     
 
 # doctor search page >>> book appointment
