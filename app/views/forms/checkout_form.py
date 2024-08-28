4c830b853e13f07a4968b360409302ec22ef3f5e
manonaSamir <bena.yalla@yahoo.com>
2024-08-28 22:43:02 +0300

    remove validate email

--- app/views/forms/checkout_form.py
+++ app/views/forms/checkout_form.py
@@ -6,18 +6,6 @@ from app import translate
 
 
 class checkoutForm(FlaskForm):
-    def validate_email_address(self, email_address_to_check):
-        email_address = User.query.filter_by(email=email_address_to_check.data).first()
-        # role = Role.query.filter_by(role_name='patient').first().id
-        # email_address = (
-        #     User.query.filter_by(email=email_address_to_check.data)
-        #     .join(UserRole)
-        #     .filter(UserRole.role_id == role)
-        #     .first()
-        # )
-        if email_address:
-            raise ValidationError(translate('email address already exists!'))
-
     firstname = StringField(validators=[Length(min=2, max=30), DataRequired()])
     lastname = StringField(validators=[Length(min=2, max=70), DataRequired()])
     email_address = StringField(validators=[Email(), DataRequired()])
