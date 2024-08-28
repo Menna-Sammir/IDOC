ebed61a12053763946870b2fafc2caa2cb3f9e59
manonaSamir <bena.yalla@yahoo.com>
2024-08-28 22:40:11 +0300

    add app at repo

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_principal import Principal
+
+
+load_dotenv()
+app = Flask(__name__)
+
+IDOC_USER = os.getenv('IDOC_USER')
+IDOC_PWD = os.getenv('IDOC_PWD')
+IDOC_HOST = os.getenv('IDOC_HOST')
+IDOC_DB = os.getenv('IDOC_DB')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{IDOC_USER}:{IDOC_PWD}@{IDOC_HOST}/{IDOC_DB}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'ad983778da711747f7cb3e3b'
+db = SQLAlchemy(app)
+
+bcrypt= Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+
+principal = Principal(app)
+
+from app.views import main
