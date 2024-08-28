ddfa8d4bd69e879b1fe64dbf652ebeedfdb11e87
manonaSamir <bena.yalla@yahoo.com>
2024-08-28 22:43:12 +0300

    add preception and profile setting

--- app/views/main.py
+++ app/views/main.py
@@ -74,13 +74,10 @@ def login_page():
     if request.method == 'POST':
         if form.validate_on_submit():
             attempted_user = User.query.filter_by(email=form.email_address.data).first()
-            print('dddddddddddddddddddddd')
             if not attempted_user:
                 attempted_user = (
                     Doctor.query.filter_by(iDNum=form.email_address.data).first().users
                 )
-
-                print('dddddddddddddddrrrrrrrrrrrrrrrrr')
             if attempted_user.activated or not attempted_user.temp_password:
                 if attempted_user and attempted_user.check_password_correction(
                     attempted_password=form.password.data
@@ -197,10 +194,3 @@ def reset_password(email):
 @app.errorhandler(403)
 def permission_denied(e):
     return 'Permission Denied', 403
-
-
-# doctor dashboard page >>> view appointments today
-@app.route('/patient_dashboard', methods=['GET', 'POST'])
-@login_required
-def patient_dash():
-    return render_template('patient-dashboard.html')
