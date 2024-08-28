651e620ecf01b1886891231aca77138db2e36cf3
adhamelsayed2000 <adhamadhoom9@gamil.com>
2024-08-28 22:41:00 +0300

    add drobdown

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+import sqlalchemy
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask import Flask, render_template
+from flask_migrate import Migrate
+
+load_dotenv()
+app = Flask(__name__)
+
+IDOC_USER = os.getenv('IDOC_USER')
+IDOC_PWD = os.getenv('IDOC_PWD')
+IDOC_HOST = os.getenv('IDOC_HOST')
+IDOC_DB = os.getenv('IDOC_DB')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqldb://{IDOC_USER}:{IDOC_PWD}@{IDOC_HOST}/{IDOC_DB}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'ad983778da711747f7cb3e3b'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+bcrypt= Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+
+from app.views import home
+
