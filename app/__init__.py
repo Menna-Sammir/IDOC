5c406d67683a84e83522d1f9cbf619795dd1d68f
manonaSamir <bena.yalla@yahoo.com>
2024-08-29 18:41:29 +0300

    add emails and redirect to doctor dashboard

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import os
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_principal import Principal
-
+from flask_mail import Mail
 
 load_dotenv()
 app = Flask(__name__)
@@ -14,7 +14,7 @@ IDOC_USER = os.getenv('IDOC_USER')
 IDOC_PWD = os.getenv('IDOC_PWD')
 IDOC_HOST = os.getenv('IDOC_HOST')
 IDOC_DB = os.getenv('IDOC_DB')
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{IDOC_USER}:{IDOC_PWD}@{IDOC_HOST}/{IDOC_DB}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqldb://{IDOC_USER}:{IDOC_PWD}@{IDOC_HOST}/{IDOC_DB}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'ad983778da711747f7cb3e3b'
 db = SQLAlchemy(app)
@@ -25,4 +25,15 @@ login_manager.login_view = "login_page"
 
 principal = Principal(app)
 
+
+app.config['MAIL_SERVER']=os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] =False
+mail = Mail(app)
+
 from app.views import main
+from app.views import patient
+from app.views import test
