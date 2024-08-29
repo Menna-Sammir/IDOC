5f4d94887b56b5cc3b6ecfa9d4bdfd35867d9ca0
fatma soliman <fatmacivil@hotmail.com>
2024-08-29 14:43:25 +0300

    search

--- app/views/doctor.py
+++ app/views/doctor.py
@@ -2,6 +2,7 @@ from app import app, db
 from flask import render_template, redirect, url_for, flash, request, jsonify
 from datetime import datetime
 from sqlalchemy import func, select
+from flask import session
 from app.models.models import Specialization, User, Doctor,  Patient, Appointment, Clinic, Message,  Governorate, Role
 
 
@@ -16,7 +17,7 @@ def search_doctor():
     governorates = Governorate.query.all()
     
     if request.method == 'POST':
-        specialization_ids = request.form.getlist('select_specialization')
+        specialization_ids = request.form.getlist('select_specialization[]')
         date = request.form.get('date')
         
         print("Specialization IDs:", specialization_ids)
@@ -30,51 +31,28 @@ def search_doctor():
             subquery = db.session.query(Appointment.doctor_id).filter(
                 func.date(Appointment.date) == search_date).group_by(
                 Appointment.doctor_id).having(func.count(Appointment.id) < 5).subquery()
-            query = query.filter(Doctor.id.in_(subquery))
+            query = query.filter(Doctor.id.in_(select(subquery)))
 
     doctors = query.all()
     print("Doctors:", doctors)  # Debug statement to check the query results
     return render_template('search.html', doctors=doctors, specializations=specializations, governorates=governorates)
 
 
-# @app.route('/search_doctor', methods=['GET', 'POST'])
-# def search_doctor():
-#     specialization_name = request.args.getlist('specialization_name')
-#     doctor_name = request.args.get('doctor_name')
-#     clinic_address = request.args.get('clinic_address')
-#     date = request.args.get('date')
-#     max_appointments = 5
+@app.route('/book_appointment', methods=['POST'])
+def book_appointment():
+    if request.method == 'POST':
+        doctor_id = request.form.get('doctor_id')
+        session['doctor_id'] = doctor_id
+        return redirect('booking.html')
 
-#     query = db.session.query(Doctor, Specialization, Clinic, Governorate) \
-#         .join(Specialization, Doctor.specialization_id == Specialization.id) \
-#         .join(Clinic, Doctor.clinic_id == Clinic.id) \
-#         .join(Governorate, Clinic.governorate_id == Governorate.id)
+# @app.route('/doctor_dashboard', methods=['GET', 'POST'])
+# def doctor_dashboard():
 
-#     if specialization_name:
-#         query = query.filter(Doctor.specialization_id.in_(specialization_name))
-#     if doctor_name:
-#         query = query.filter(Doctor.name.like(f'%{doctor_name}%'))
-#     if clinic_address:
-#         query = query.filter(Clinic.address.like(f'%{clinic_address}%'))
-#     if date:
-#         search_date = datetime.strptime(date, '%Y-%m-%d').date()
-#         query = query.filter(~Doctor.appointments.any(func.date(Appointment.date) == search_date))
 
-#     doctors = query.all()
-#     specializations = Specialization.query.all()
-#     governorates = Governorate.query.all()
-    
-#     if request.method == 'POST':
-#         specialization_id = request.get('select_specialization')
-#         date = request.get('date')
-        
-#         if specialization_id:
-#             query = query.filter(Doctor.specialization_id == specialization_id)
-#         if date:
-#             pass
-#         query = db.session.query(Doctor, Clinic, Governorate).join(Clinic).join(Governorate)
-        
-#     return render_template('search.html', doctors=doctors, specializations=specializations, governorates=governorates)
 
-    
-        
+
+
+
+
+
+
