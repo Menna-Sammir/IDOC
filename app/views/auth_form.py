909f458422a77ee476059a5ba9a1a2356224dde0
manonaSamir <bena.yalla@yahoo.com>
2024-08-29 18:46:53 +0300

    clinic Signup and logout

--- app/views/auth_form.py
+++ app/views/auth_form.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField, SubmitField, SelectField
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
-from app.models.models import User, Doctor
+from app.models.models import *
 
-class RegisterForm(FlaskForm):
+class RegisterDocForm(FlaskForm):
     def validate_username(self, username_to_check):
         user = User.query.filter_by(name= username_to_check.data).first()
         if user:
@@ -27,8 +27,32 @@ class RegisterForm(FlaskForm):
     doctor_id = SelectField(label='select doctor', validators=[DataRequired()])
     submit = SubmitField(label='Create Account')
 
+class RegisterClinicForm(FlaskForm):
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(name= username_to_check.data).first()
+        if user:
+            raise ValidationError('username already exists!')
+
+    def validate_clinic(self, clinic_to_check):
+        clinic = Clinic.query.filter_by(clinic_id= clinic_to_check.data).first()
+        if not clinic:
+            raise ValidationError('clinic not exists!')
+
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email= email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('email address already exists!')
+
+
+    username =StringField(label='User Name', validators=[Length(min=2,max=30), DataRequired()])
+    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password', validators=[EqualTo('password1'), DataRequired()])
+    clinic_id = SelectField(label='select clinic', validators=[DataRequired()])
+    submit = SubmitField(label='Create Account')
+
 
 class LoginForm(FlaskForm):
-    username = StringField(label='User Name', validators=[DataRequired()])
+    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
     password = PasswordField(label='Password', validators=[DataRequired()])
-    submit = SubmitField(label='sign in')
+    submit = SubmitField(label='log in')
