eaa15b245d04e064e466fa42fdc78e1066aa7be7
adhamelsayed2000 <adhamadhoom9@gamil.com>
2024-08-29 18:48:33 +0300

    add booking

---
+++ app/views/booking.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import RadioField, SubmitField
+from wtforms.validators import DataRequired
+
+class AppointmentForm(FlaskForm):
+    timeslots = RadioField('Available Timeslots', choices=[], validators=[DataRequired()])
+    submit = SubmitField('Book Appointment')
--- app/views/home.py
+++ app/views/home.py
@@ -7,21 +7,26 @@ from sqlalchemy import not_
 from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from app.models.models import Specialization
-
+from app.models.models import Specialization, Doctor, Clinic, Governorate, Appointment
+from app.views.search import SearchForm
+from datetime import datetime, timedelta
+from app.views.booking import AppointmentForm
 
 @app.route('/')
-@app.route('/home')
-def home_page():
-    specializations = Specialization.query.all()
-    specializations_valu = [(spe.id, spe.specialization_name) for spe in specializations]
-    print(specializations_valu)
-    return render_template('index.html', specializations=specializations_valu)
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    form = SearchForm()
+    form.specialization.choices = [(s.id, s.specialization_name) for s in Specialization.query.all()]
+    form.governorate.choices = [(g.id, g.governorate_name) for g in Governorate.query.all()]
 
+    if form.validate_on_submit():
+        specialization_id = form.specialization.data
+        governorate_id = form.governorate.data
+        doctor_name = form.doctor_name.data
+        return redirect(url_for('search_results', specialization_id=specialization_id, governorate_id=governorate_id, doctor_name=doctor_name))
+    
+    return render_template('index.html', form=form)
 
-@app.route('/test')
-def test_page():
-    return render_template('search.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -64,3 +69,46 @@ def doctor_signup_page():
 def login_page():
     form = LoginForm()
     return render_template('login.html', form=form)
+
+@app.route('/booking/<string:doctor_id>', methods=['GET', 'POST'])
+def doctor_appointments(doctor_id):
+    form = AppointmentForm()
+    if request.method == 'GET':
+        
+        doctor = Doctor.query.get_or_404(doctor_id)
+        clinic = doctor.clinic
+    
+        dates = []
+        for i in range(6):
+            date = datetime.now() + timedelta(days=i)
+            dates.append((date.strftime('%Y-%m-%d'), date.strftime('%A')))
+        
+    
+        working_hours = clinic.working_hours.split(',')  
+        
+    
+        timeslots = []
+        for date in dates:
+            for hour in working_hours:
+                timeslot = f"{date[0]} {hour.strip()}"
+                timeslots.append((timeslot, f"{date[1]} - {hour.strip()}"))
+        
+        form.timeslots.choices = timeslots
+        
+
+    if form.validate_on_submit():
+        selected_timeslot = form.timeslots.data
+        appointment = Appointment(
+            date=selected_timeslot.split()[0],
+            time=selected_timeslot.split()[1],
+            status=False,
+            seen=False,
+            clinic_id=clinic.id,
+            doctor_id=doctor.id
+        )
+        db.session.add(appointment)
+        db.session.commit()
+        flash('Appointment booked successfully!', 'success')
+        return redirect(url_for('doctor_appointments', doctor_id=doctor.id))
+    
+    return render_template('booking.html', form=form, doctor=doctor, dates=dates, clinic=clinic)
