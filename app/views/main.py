b60ed3962b70ffc89eda9c7d20317b1e62cab2fe
manonaSamir <bena.yalla@yahoo.com>
2024-08-29 14:43:27 +0300

    add userid to patient

--- app/views/main.py
+++ app/views/main.py
@@ -36,6 +36,7 @@ def signup_page():
                 )
                 patient_create = Patient(
                     phone = form.phone.data,
+                    users = user_to_create
                 )
                 patient_role = Role.query.filter_by(role_name='patient').first_or_404()
                 role_to_create = UserRole(role_id=patient_role.id, user=user_to_create)
