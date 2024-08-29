118dd0a90502956a6f73eacbd2722ea758501e5b
manonaSamir <bena.yalla@yahoo.com>
2024-08-29 14:43:26 +0300

    finish all cycle

--- app/views/admin.py
+++ app/views/admin.py
@@ -1,13 +1,8 @@
 from app import app, db
-from flask import render_template, redirect, url_for, flash, request, jsonify
-from datetime import datetime
-from sqlalchemy import func, select
-from sqlalchemy.orm import joinedload
-from flask_login import login_required, current_user
-from flask import session
-from datetime import date
-from app.models.models import Specialization, User, Doctor,  Patient, Appointment, Clinic, Message,  Governorate, Role
-from flask_principal import Permission, RoleNeed, Identity, AnonymousIdentity, identity_loaded, identity_changed
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_required
+from app.models.models import Specialization, Doctor, Clinic, Governorate
+from flask_principal import Permission, RoleNeed
 from werkzeug.utils import secure_filename
 import uuid
 from app.views.forms.addClinic_form import ClinicForm
@@ -20,7 +15,6 @@ doctor_permission = Permission(RoleNeed('doctor'))
 clinic_permission = Permission(RoleNeed('clinic'))
 
 
-
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 
@@ -29,22 +23,33 @@ def allowed_file(filename):
 
 
 # admin dashboard page >>> view appointments today
-@app.route('/admin_dashboard', methods=['GET', 'POST'], strict_slashes=False, endpoint='dashboard')
-# @login_required
-# @admin_permission.require(http_exception=403)
+@app.route(
+    '/admin_dashboard',
+    methods=['GET', 'POST'],
+    strict_slashes=False,
+    endpoint='dashboard'
+)
+@login_required
+@admin_permission.require(http_exception=403)
 def admin_dash():
-    doctor_details=db.session.query(Doctor).all()
-    clinic_details=db.session.query(Clinic).all()
+    doctor_details = db.session.query(Doctor).all()
+    clinic_details = db.session.query(Clinic).all()
     doctor_count = db.session.query(Doctor).count()
     clinic_count = db.session.query(Clinic).count()
-    return render_template('admin-dashboard.html',
-                           doctor_details=doctor_details,
-                           clinic_details=clinic_details,
-                           doctor_count=doctor_count,
-                           clinic_count=clinic_count)
+    return render_template(
+        'admin-dashboard.html',
+        doctor_details=doctor_details,
+        clinic_details=clinic_details,
+        doctor_count=doctor_count,
+        clinic_count=clinic_count
+    )
 
 
-@app.route('/add_clinic', methods=['GET', 'POST'], strict_slashes=False, endpoint='add_clinic')
+@login_required
+@admin_permission.require(http_exception=403)
+@app.route(
+    '/add_clinic', methods=['GET', 'POST'], strict_slashes=False, endpoint='add_clinic'
+)
 def add_clinic():
     add_clinic_form = ClinicForm()
     govs = Governorate.query.filter().all()
@@ -81,17 +86,16 @@ def add_clinic():
                         return redirect(request.url)
                     unique_str = str(uuid.uuid4())[:8]
                     original_filename, extension = os.path.splitext(file.filename)
-                    new_filename = (
-                        f"{unique_str}_{add_clinic_form.clinicName.data.replace(' ', '_')}{extension}"
-                    )
+                    new_filename = f"{unique_str}_{add_clinic_form.clinicName.data.replace(' ', '_')}{extension}"
                     Clinic_create.photo = new_filename
 
                     if file and allowed_file(file.filename):
                         filename = secure_filename(new_filename)
                         file.save(
-                            os.path.join(app.config['UPLOAD_FOLDER'], 'clinic', filename)
+                            os.path.join(
+                                app.config['UPLOAD_FOLDER'], 'clinic', filename
+                            )
                         )
-
                     db.session.add(Clinic_create)
                     db.session.commit()
                     return redirect(url_for('dashboard'))
@@ -107,7 +111,11 @@ def add_clinic():
     return render_template('add-clinic.html', form=add_clinic_form)
 
 
-@app.route('/add_doctor', methods=['GET', 'POST'], strict_slashes=False, endpoint='add_doctor')
+@login_required
+@admin_permission.require(http_exception=403)
+@app.route(
+    '/add_doctor', methods=['GET', 'POST'], strict_slashes=False, endpoint='add_doctor'
+)
 def add_doctor():
     add_doctor_form = DoctorForm()
     clinics = Clinic.query.filter().all()
@@ -121,9 +129,11 @@ def add_doctor():
     ]
     if request.method == 'POST':
         if add_doctor_form.validate_on_submit():
-            doctor_name = add_doctor_form.firstname.data + ' ' + add_doctor_form.lastname.data
+            doctor_name = (
+                add_doctor_form.firstname.data + ' ' + add_doctor_form.lastname.data
+            )
             doctor = Doctor.query.filter_by(
-                name=doctor_name, clinic_id =add_doctor_form.clinic_id.data
+                name=doctor_name, clinic_id=add_doctor_form.clinic_id.data
             ).first()
 
             if doctor:
@@ -132,12 +142,12 @@ def add_doctor():
                 try:
                     clinic = Clinic.query.get(add_doctor_form.clinic_id.data).name
                     Doctor_create = Doctor(
-                    name = doctor_name,
-                    phone = add_doctor_form.phone.data,
-                    email = add_doctor_form.email_address.data,
-                    price = add_doctor_form.price.data,
-                    specialization_id = add_doctor_form.specialization_id.data,
-                    clinic_id = add_doctor_form.clinic_id.data
+                        name=doctor_name,
+                        phone=add_doctor_form.phone.data,
+                        email=add_doctor_form.email_address.data,
+                        price=add_doctor_form.price.data,
+                        specialization_id=add_doctor_form.specialization_id.data,
+                        clinic_id=add_doctor_form.clinic_id.data
                     )
                     if 'photo' not in request.files:
                         flash('No file part')
@@ -148,13 +158,15 @@ def add_doctor():
                         return redirect(request.url)
                     unique_str = str(uuid.uuid4())[:8]
                     original_filename, extension = os.path.splitext(file.filename)
-                    new_filename = (
-                        f"{unique_str}_{clinic.replace(' ', '_')}_{doctor_name.replace(' ', '_')}{extension}"
-                    )
+                    new_filename = f"{unique_str}_{clinic.replace(' ', '_')}_{doctor_name.replace(' ', '_')}{extension}"
                     Doctor_create.photo = new_filename
                     if file and allowed_file(file.filename):
                         filename = secure_filename(new_filename)
-                        file.save(os.path.join(app.config['UPLOAD_FOLDER'], "doctors", filename))
+                        file.save(
+                            os.path.join(
+                                app.config['UPLOAD_FOLDER'], 'doctors', filename
+                            )
+                        )
                     db.session.add(Doctor_create)
                     db.session.commit()
                     return redirect(url_for('dashboard'))
