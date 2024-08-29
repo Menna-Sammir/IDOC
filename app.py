76f59d20510a840efc0f5c3d30bf2e2a9efcce60
manonaSamir <bena.yalla@yahoo.com>
2024-08-29 18:49:55 +0300

    refactor app

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ from app.models.adminUser import *
 from app.models.addGovs import *
 from app.models.addspecalties import *
 
-if __name__ == '__main__':
+
+def initialize_app():
     with app.app_context():
         db.create_all()
         seed_governorates()
         create_admin_user()
         create_specialties()
+
+
+
+if __name__ == '__main__':
+    initialize_app()
     port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable if available
     socketio.run(app, host='0.0.0.0', port=port, debug=True)
