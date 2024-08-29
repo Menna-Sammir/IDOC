ed6883652e4937ed6e0541e109c7ccc852ff4c15
manonaSamir <bena.yalla@yahoo.com>
2024-08-29 18:43:19 +0300

    finish home page

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from app import app, db, socketio
 from flask_session import Session
 from app.models.adminUser import *
 from app.models.addGovs import *
+from app.models.addspecalties import *
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         seed_governorates()
         create_admin_user()
+        create_specialties()
         
     socketio.run(app, host='0.0.0.0', debug=True)
