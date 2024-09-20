36a667e2d3e64432e6684a12ced69465125025be
adhamelsayed2000 <adhamadhoom9@gamil.com>
2024-09-20 18:38:42 +0300

    delet

--- app/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    SECRET_KEY = 'your_secret_key'
-    DEBUG = False
-
-class TestingConfig(Config):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # استخدام قاعدة بيانات في الذاكرة للاختبارات
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
