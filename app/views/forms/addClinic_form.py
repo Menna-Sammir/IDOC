53cc0432e2fde5e1bf444d5f134d94ec7dc072f0
manonaSamir <bena.yalla@yahoo.com>
2024-09-25 13:08:30 +0300

    add clinic

--- app/views/forms/addClinic_form.py
+++ app/views/forms/addClinic_form.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField, SubmitField, SelectField, TextAreaField
-from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
-from app.models.models import *
-from wtforms_components import TimeField
-from flask_wtf.file import FileField
-from flask_wtf.csrf import CSRFProtect
-
-
-csrf = CSRFProtect()
-class ClinicForm(FlaskForm):
-
-    def validate_email_address(self, email_address_to_check):
-        email_address = Clinic.query.filter_by(email= email_address_to_check.data).first()
-        if email_address:
-            raise ValidationError('email address already exists!')       
-=======
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField, TextAreaField
@@ -40,26 +22,10 @@ class ClinicForm(FlaskForm):
         email_address = User.query.filter_by(email=email_address_to_check.data).first()
         if email_address:
             raise ValidationError(translate('email address already exists!'))
->>>>>>> 43f670543734e42f1cbe595ce9a8b1d215f97291
 
     def file_size_check(self, logo):
         if logo.data:
             if len(logo.data.read()) > 2 * 1024 * 1024:
-<<<<<<< HEAD
-                raise ValidationError('File size must be less than 2MB.')
-            logo.data.seek(0)
-
-    clinicName =StringField(label='Clinic Name', validators=[Length(min=2,max=30), DataRequired()])
-    clinicAddress =TextAreaField(label='Clinic Address', validators=[Length(min=2,max=90), DataRequired()])
-    email_address = StringField(label='Email Address', validators=[Email(), DataRequired(),Length(max=50)])
-    gov_id = SelectField(label='governorate', validators=[DataRequired()])
-    phone = StringField(label='Phone', validators=[Length(min=0, max=11)])
-    fromHour=TimeField(label='From', validators=[DataRequired()])
-    toHour=TimeField(label='To', validators=[DataRequired()])
-    logo=FileField(label='Clinic Logo', validators=[DataRequired(),file_size_check])
-
-    submit = SubmitField(label='Add Clinic')
-=======
                 raise ValidationError(translate('File size must be less than 2MB.'))
             logo.data.seek(0)
 
@@ -104,4 +70,3 @@ class EditClinicForm(FlaskForm):
         self.submit.label.text = translate('Save Changes')
 
 
->>>>>>> 43f670543734e42f1cbe595ce9a8b1d215f97291
